9e517d246bd651e68c6c6bd87fbb4619cb2e536c
timoweave <timoweave@users.noreply.github.com>
2018-07-03 14:27:23 -0400

    remove access keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   region: "us-east-1",
   bucket: "timoroof-s3-uploads-123",
-  accessKeyId: "AKIAJMREYRQY5AXJCCNQ",
-  secretAccessKey: "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",
+  accessKeyId: "removed",
+  secretAccessKey: "removed",
   maxUploadCount: 10,
   port: 8001,
 };
